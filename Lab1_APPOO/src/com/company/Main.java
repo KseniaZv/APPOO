0fd59cde287a415b50c64527d1c42700afd6233f
KseniaZv <xeniazvercova@gmail.com>
2018-02-23 09:19:39 +0200

    Som changes in Main

--- Lab1_APPOO/src/com/company/Main.java
+++ Lab1_APPOO/src/com/company/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
         Man man = new Man();
 
-        man.setWeight(88);
+        man.setWeight(83);
         man.setHeight(180);
-        man.setAge(30);
+        man.setAge(20);
 
         System.out.println("Man's Basal Metabolic Rate is: " + man.BMR() + "\n");
 
@@ -36,9 +36,9 @@ public class Main {
 
         Woman woman = new Woman();
 
-        woman.setWeight(61);
-        woman.setHeight(168);
-        woman.setAge(44);
+        woman.setWeight(65);
+        woman.setHeight(175);
+        woman.setAge(21);
 
         System.out.println("Woman's Basal Metabolic Rate is: " + woman.BMR() + "\n" );
 
@@ -47,16 +47,16 @@ public class Main {
         MealFactory mealFactoryW = new MealFactory();
 
         Meal meal_1w = mealFactoryW.getMeal("BREAKFAST");
-        meal_1w.eat(man.BMR());
+        meal_1w.eat(woman.BMR());
 
         Meal meal_2w = mealFactoryW.getMeal("DINNER");
-        meal_2w.eat(man.BMR());
+        meal_2w.eat(woman.BMR());
 
         Meal meal_3w = mealFactoryW.getMeal("SNACK");
-        meal_3w.eat(man.BMR());
+        meal_3w.eat(woman.BMR());
 
         Meal meal_4w = mealFactoryW.getMeal("SUPPER");
-        meal_4w.eat(man.BMR());
+        meal_4w.eat(woman.BMR());
 
     }
 }
