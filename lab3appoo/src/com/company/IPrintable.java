ebd1ea3aa742bcefdabc3889e8284b46b96ff4ef
KseniaZv <xeniazvercova@gmail.com>
2018-03-23 09:09:34 +0200

    lab3 deleted

--- lab3appoo/src/com/company/IPrintable.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public interface IPrintable {
-
-    void  print(Object object);
-}
--- lab3appoo/src/com/company/IZodiac.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public interface IZodiac {
-
-    String defineZodiac(int day, int month);
-}
